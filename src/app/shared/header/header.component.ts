f13f965b2f309780ee3548e3ee5ee81b75fe5376
darsruiz <dars2@live.com>
2022-06-11 08:22:20 +0200

    header redone, added/fixed translations

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -11,14 +11,27 @@ export class HeaderComponent implements OnInit {
   constructor(private service: HandleDataService) { }
 
   languages = ['en', 'es'];
+  activeLang: string | undefined;
   ngOnInit(): void {
+    this.service.eventCallback$
+      .subscribe(x => this.setActiveLang(x));
+
+    this.setBrowserLang();
+  }
+
+  setBrowserLang() {
     let lang = navigator.language.slice(0, 2).toLowerCase()
     if (lang == ('es' || 'ca')) {
       this.setLang('es')
     }
+    this.activeLang = lang;
   }
 
   setLang(lang: string) {
     this.service.setLang(lang);
+  }
+
+  setActiveLang(lang: string) {
+    this.activeLang = lang;
   }
 }
